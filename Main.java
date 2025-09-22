94b646618f0246bbe3573d3484a45dc901f0ecc8
skittlecamp <81906603+skittlecamp@users.noreply.github.com>
2025-09-22 13:02:30 +0200

    new code

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("vyska: ");
+            int height = scanner.nextInt();
+
+            for (int i = 1; i <= height; i++) {
+
+                for (int j = 1; j <= height - i; j++) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++) {
+                    System.out.print("*");
+                }
+
+                System.out.println();
+            }
+
+            scanner.close();
+        }
+    }
+
